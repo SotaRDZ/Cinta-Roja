e5a348764ccd9063ca184304cfb1da370696d873
SoraRdz <irmagrodriguezv@hotmail.com>
2019-10-22 20:09:32 -0500

    Update repaso.js
    guardé una variable numerica

--- Repaso/repaso.js
+++ Repaso/repaso.js
@@ -1,2 +1,4 @@
 // Variables 
-const string = 'Hola Mundo'; // const---> nunca va a cambiar su valor - string ---> comando de cadena
+const string = 'Hola Mundo'; // cConst ---> nunca va a cambiar su valor - String ---> comando de cadena
+
+let entero = 63; // Let ----> Solo se puede usar en el scope creado
