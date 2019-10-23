243811a6942708bf7cb1a3404f9ca15716d13d08
SoraRdz <irmagrodriguezv@hotmail.com>
2019-10-22 20:16:10 -0500

    Update repaso.js
    Flotante y booleano

--- Repaso/repaso.js
+++ Repaso/repaso.js
@@ -1,4 +1,16 @@
 // Variables 
-const string = 'Hola Mundo'; // cConst ---> nunca va a cambiar su valor - String ---> comando de cadena
+const string = 'Hola Mundo'; // Const ---> nunca va a cambiar su valor - String ---> comando de cadena
 
 let entero = 63; // Let ----> Solo se puede usar en el scope creado
+
+var flotante = 7485.38 // Var ---> Se usa de manera global
+
+const hola = () => {
+    let saludo ='Hola'
+}
+
+const adios =() => {
+    let despedir = 'Adios'
+}
+
+const booleano = false; 
